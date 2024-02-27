f08bc5962fd5b37311e2c8a80a5621d970202f72
Marina Kononova <marinakononova@mail.ru>
2024-02-27 15:28:52 +0400

    Rename percent to Percent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args){
         BonusService service = new BonusService();
 
-        int expected = 150;
-        long actual = service.calculate(5_000, true);
-        System.out.println("1) " + expected + " == ? == " + actual);
+
+        long bonus = service.calculate(5_000, true);
+        System.out.println(bonus);
 
     }
 }
